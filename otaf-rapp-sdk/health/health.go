1cd21271d747911650c49096320d47f317958456
sushantmundepi <sushantmundepi@gmail.com>
2025-11-26 10:04:38 +0530

    health: define checker
    Co-Authored-By: Shivam9790 <shivamgangwar9463@gmail.com>

---
+++ otaf-rapp-sdk/health/health.go
@@ -0,0 +1,30 @@
+// Copyright 2025-2026 coRAN LABS Private Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package health tracks the reachability of the platform services an rApp
+// depends on and exposes the result through the rApp's status endpoint.
+package health
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Checker interface {
+	Name() string
+	Check(ctx context.Context) error
+}
