1177dabc411b6a9f2d3725c8a7d985052c0c848c
sushantmundepi <sushantmundepi@gmail.com>
2025-12-08 16:18:28 +0530

    ingest/httpsrc: add tests for serve
    Co-Authored-By: Shivam9790 <shivamgangwar9463@gmail.com>

---
+++ otaf-rapp-sdk/ingest/httpsrc/httpsrc_test.go
@@ -0,0 +1,34 @@
+// Copyright 2025-2026 coRAN LABS Private Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package httpsrc
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gorilla/mux"
+
+	"github.com/coranlabs/OTAF/otaf-rapp-sdk/ingest"
+)
+
+func serve(s *Source) http.Handler {
+	r := mux.NewRouter()
+	s.Register(r)
+	return r
+}
