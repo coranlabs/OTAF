96d19c3df5ca58153e780c568e273d6c0b5b9b94
sushantmundepi <sushantmundepi@gmail.com>
2026-02-05 15:49:02 +0530

    sdnr: introduce decl0

---
+++ otaf-rapp-sdk/sdnr/client.go
@@ -0,0 +1,38 @@
+// Copyright 2025-2026 coRAN LABS Private Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package sdnr reaches the managed network over RESTCONF through the SMO's
+// controller, which fronts each node's O1 NETCONF session.
+package sdnr
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	rappsdk "github.com/coranlabs/OTAF/otaf-rapp-sdk"
+	"github.com/coranlabs/OTAF/otaf-rapp-sdk/errs"
+)
+
+const (
+	yangJSON       = "application/yang-data+json"
+	snippetLen     = 512
+	defaultTimeout = 30 * time.Second
+)
