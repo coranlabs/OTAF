d805a144535b3044ae5d44c756f60c181f9ddf3a
sushantmundepi <sushantmundepi@gmail.com>
2026-03-24 20:54:53 +0530

    csar: add uuid
    Co-Authored-By: Shivam9790 <shivamgangwar9463@gmail.com>

---
+++ otaf-rapp-sdk/csar/instances.go
@@ -0,0 +1,31 @@
+// Copyright 2025-2026 coRAN LABS Private Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package csar
+
+import (
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+)
+
+func UUID() (string, error) {
+	b := make([]byte, 16)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	b[6] = (b[6] & 0x0f) | 0x40
+	b[8] = (b[8] & 0x3f) | 0x80
+	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:16]), nil
+}
