6655566ffd7b7c0e606bcb8e8f9e99b048e3a5a7
sushantmundepi <sushantmundepi@gmail.com>
2026-04-08 17:10:24 +0530

    cmd/rappctl: add main
    Co-Authored-By: Shivam9790 <shivamgangwar9463@gmail.com>

---
+++ otaf-rapp-sdk/cmd/rappctl/main.go
@@ -0,0 +1,56 @@
+// Copyright 2025-2026 coRAN LABS Private Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Command rappctl creates rApps and turns them into packages the Non-RT RIC
+// can onboard.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(2)
+	}
+
+	var err error
+	switch os.Args[1] {
+	case "new":
+		err = runNew(os.Args[2:])
+	case "package":
+		err = runPackage(os.Args[2:])
+	case "validate":
+		err = runValidate(os.Args[2:])
+	case "hashpw":
+		err = runHashpw(os.Args[2:])
+	case "version", "-v", "--version":
+		err = runVersion()
+	case "help", "-h", "--help":
+		usage()
+		return
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command %q\n\n", os.Args[1])
+		usage()
+		os.Exit(2)
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
